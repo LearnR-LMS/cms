32392bead680d47313fe6d26320607a4c4c4aa79
Via Jackson <vanvia.4462@gmail.com>
2022-07-18 13:44:06 +0700

    send earning when a course is finished

--- app/Services/EFoxUserService.php
+++ app/Services/EFoxUserService.php
@@ -5,6 +5,7 @@ namespace App\Services;
 use App\Models\EFCourse;
 use App\Models\EFScore;
 use App\Models\User;
+use GuzzleHttp\Client;
 
 class EFoxUserService extends BaseService
 {
@@ -57,13 +58,14 @@ class EFoxUserService extends BaseService
     }
 
     // Courses
-    public function getListCourse($request){
+    public function getListCourse($request)
+    {
         // $page = (int)$request->get("page");
         // if()
         $data = EFCourse::paginate(config('constants.paging_display'));
         return $data;
     }
-    
+
     public function storeCourse($request)
     {
         if (EFCourse::find($request->u_id)) abort(422, "ID khóa học đã tồn tại.");
@@ -106,7 +108,7 @@ class EFoxUserService extends BaseService
     public function scoreCourse($request)
     {
         $user_id = $request->input('user_id');
-        if (!User::query()->find($user_id))
+        if (!$user = User::query()->find($user_id))
             abort(404, "Không tìm thấy user.");
 
         $course_id = $request->input('course_id');
@@ -114,15 +116,65 @@ class EFoxUserService extends BaseService
             abort(404, "Không tìm thấy khóa học.");
 
         $score = $request->input('score');
-        
+
         $input = [
             'ef_course_id' => $course_id,
             'user_id' => $user_id,
             'score' => $score
         ];
 
-        EFScore::create($input);
         // $ef_course->user()->attach($user_id, ['score'=> $score ]);
+        $ef_score = EFScore::query()->updateOrCreate(
+            [
+                'ef_course_id' => $course_id,
+                'user_id' => $user_id,
+            ],
+            $input
+        );
+
+        // Call dApp
+        if ($user->address_vallet && $user->pen_id && $ef_score->earn_status == EFScore::PENDING_EARN) {
+            // Call check using which pen
+            $pens = new Client();
+            $uri_get_pens = config('lr.api_url') . "/token/" . $user->address_vallet . "?page=1";
+            $res_pens = $pens->request('GET', $uri_get_pens, [
+                'headers' => [
+                    'Accept'     => 'application/json',
+                    'Authorization'      => config('lr.token')
+                ]
+            ]);
+            $stt_pens = $res_pens->getStatusCode(); // 200
+            $body_pens = json_decode($res_pens->getBody());
+            $pen_list = $body_pens->data->docs;
+            $pen_used = false;
+            foreach ($pen_list as $p) {
+                if ($p->index == $user->pen_id) {
+                    $pen_used = true;
+                    break;
+                }
+            }
+
+            // Call earn coin
+            if ($stt_pens == 200 && $pen_used) {
+                $earning = new Client();
+                $res_earning = $earning->request('POST', config('lr.api_url') . '/earn/token/quiz', [
+                    'headers' => [
+                        'Accept'     => 'application/json',
+                        'Authorization'      => config('lr.token')
+                    ],
+                    'json' => [
+                        "received_address" => $user->address_vallet,
+                        "point" => $score / 100,
+                        "pen_index" => $user->pen_id,
+                        "total_time_of_course" => ($ef_course->total_time_learning) * 60
+                    ]
+                ]);
+                $stt_earning = $res_earning->getStatusCode(); // 200
+                if ($stt_earning == 200) {
+                    $ef_score->update(['earn_status' => EFScore::APPROVED_EARN]);
+                }
+            }
+        }
 
         return true;
     }
